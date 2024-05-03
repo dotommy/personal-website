a823dfc8bd9694599fd05baef8a18b0820ec8230
dotommy <darkingloner@gmail.com>
2024-05-03 20:21:24 +0200

    add react-ga4 and css changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './App.css'
-
 import Desk from './pages/Desk'
 
 function App() {
-  
-  return (<div className='app'><Desk/></div>)
-}
+  return (
+  <div className='app'>
+    <Desk/>
+  </div>
+)}
 
 export default App
--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -1,25 +1,42 @@
 import React, { useEffect, useState } from 'react'
 import { useProgress } from '@react-three/drei'
+import ReactGA from "react-ga4";
+import CookieConsent, { Cookies } from "react-cookie-consent";
 import './Welcome.css'
 
 function Welcome({start}) {
     const [hide, setHide] = useState('')
-
+    const [acceptCookie, setacceptCookie] = useState(false)
     const { progress } = useProgress()
+
     useEffect(() => {
-        console.log(progress)
-    })
+        const isCookieAccepted = Cookies.get('CookieConsent') === 'true';
+        setacceptCookie(isCookieAccepted);
+    }, []);
+
+    const handleAcceptCookie = () => {
+        setacceptCookie(true)
+    }
 
     function startScene() {
+        ReactGA.initialize("G-WWVWGSW5NR");
         start()
         setInterval(() => {setHide('hide')},200)
     }
 
     return (
         <div className={`welcome ${hide}`}>
+            <CookieConsent
+                onAccept={handleAcceptCookie}
+                buttonText="Accept"
+                buttonWrapperClasses="cookie-button-wrapper"
+                buttonClasses="cookie-button"
+            >
+                This website uses cookies to enhance the user experience. Click "Accept" to enable the "Enter" button
+            </CookieConsent>
             <div className='container-welcome'>
                 <div className='container-description'><div>Tommaso Frosini Website</div><div style={{position:'relative', marginLeft: '2px'}}><div className='blink'>_</div></div></div>
-                <div className='container-button'><button className='welcome-button' disabled={progress<100} onClick={startScene}>Enter</button></div>
+                <div className='container-button'><button className='welcome-button' disabled={progress < 100 || !acceptCookie} onClick={startScene}>Enter</button></div>
             </div>
         </div>
     )
