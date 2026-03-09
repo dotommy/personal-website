51655731be3ca32905d1924bd11ef4fc1de8006f
dotommy <darkingloner@gmail.com>
2026-03-09 11:08:55 +0100

    welcome fade-out, progress bar, cookie banner CRT theme

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -5,7 +5,8 @@ import CookieConsent from "react-cookie-consent";
 import './Welcome.css'
 
 function Welcome({start}) {
-    const [hide, setHide] = useState('')
+    const [fading, setFading] = useState(false)
+    const [gone, setGone] = useState(false)
     const { progress } = useProgress()
 
     const handleAcceptCookie = () => {
@@ -15,11 +16,14 @@ function Welcome({start}) {
 
     function startScene() {
         start()
-        setTimeout(() => {setHide('hide')}, 200)
+        setFading(true)
+        setTimeout(() => setGone(true), 500)
     }
 
+    if (gone) return null
+
     return (
-        <div className={`welcome ${hide}`}>
+        <div className={`welcome${fading ? ' fading' : ''}`}>
             <CookieConsent
                 onAccept={handleAcceptCookie}
                 buttonText="Accept"
@@ -29,11 +33,26 @@ function Welcome({start}) {
                 buttonWrapperClasses="cookie-button-wrapper"
                 buttonClasses="cookie-button"
                 setDeclineCookie
+                style={{
+                    background: 'black',
+                    borderTop: '3pt solid white',
+                    fontFamily: '"VT323", monospace',
+                    fontSize: '18pt',
+                    color: 'white',
+                    alignItems: 'center',
+                }}
+                contentStyle={{ margin: '0 0 8px 0' }}
             >
                 This website uses cookies for analytics. You can still enter without accepting.
             </CookieConsent>
             <div className='container-welcome'>
                 <div className='container-description'><div>Tommaso Frosini Website</div><div style={{position:'relative', marginLeft: '2px'}}><div className='blink'>_</div></div></div>
+                {progress < 100 && (
+                    <div className='progress-bar-wrapper'>
+                        <div className='progress-bar' style={{width: `${progress}%`}}></div>
+                        <span className='progress-label'>{Math.round(progress)}%</span>
+                    </div>
+                )}
                 <div className='container-button'><button className='welcome-button' disabled={progress < 100} onClick={startScene}>Enter</button></div>
             </div>
         </div>
