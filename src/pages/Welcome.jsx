a1c992f6dc2cbc49db93eefffd16447f11f89c00
dotommy <darkingloner@gmail.com>
2026-03-09 11:06:26 +0100

    GDPR compliance and GA initialization fix

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -1,25 +1,19 @@
-import React, { useEffect, useState } from 'react'
+import { useState } from 'react'
 import { useProgress } from '@react-three/drei'
 import ReactGA from "react-ga4";
-import CookieConsent, { Cookies } from "react-cookie-consent";
+import CookieConsent from "react-cookie-consent";
 import './Welcome.css'
 
 function Welcome({start}) {
     const [hide, setHide] = useState('')
-    const [acceptCookie, setacceptCookie] = useState(false)
     const { progress } = useProgress()
 
-    useEffect(() => {
-        const isCookieAccepted = Cookies.get('CookieConsent') === 'true';
-        setacceptCookie(isCookieAccepted);
-    }, []);
-
     const handleAcceptCookie = () => {
-        setacceptCookie(true)
+        ReactGA.initialize(import.meta.env.VITE_GA_MEASUREMENT_ID);
+        ReactGA.send("pageview");
     }
 
     function startScene() {
-        ReactGA.initialize("G-WWVWGSW5NR");
         start()
         setTimeout(() => {setHide('hide')}, 200)
     }
@@ -29,14 +23,18 @@ function Welcome({start}) {
             <CookieConsent
                 onAccept={handleAcceptCookie}
                 buttonText="Accept"
+                declineButtonText="Decline"
+                enableDeclineButton
+                declineButtonClasses="cookie-button"
                 buttonWrapperClasses="cookie-button-wrapper"
                 buttonClasses="cookie-button"
+                setDeclineCookie
             >
-                This website uses cookies to enhance the user experience. Click "Accept" to enable the "Enter" button
+                This website uses cookies for analytics. You can still enter without accepting.
             </CookieConsent>
             <div className='container-welcome'>
                 <div className='container-description'><div>Tommaso Frosini Website</div><div style={{position:'relative', marginLeft: '2px'}}><div className='blink'>_</div></div></div>
-                <div className='container-button'><button className='welcome-button' disabled={progress < 100 || !acceptCookie} onClick={startScene}>Enter</button></div>
+                <div className='container-button'><button className='welcome-button' disabled={progress < 100} onClick={startScene}>Enter</button></div>
             </div>
         </div>
     )
